f3b161d4654c33ce7815bc0663a186f2d1d597dc
benothman <benothmanzied6@gmail.com>
2022-02-18 14:38:11 +0100

    first commit

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    
+    #[Route('/blog', name: 'blog')]
+    public function index(): Response
+    {
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+            'x' => 100
+        ]);
+    }
+    #[Route('/', name: 'home')]
+        public function home()
+        {
+        return $this->render('blog/home.html.twig');
+        }
+}
